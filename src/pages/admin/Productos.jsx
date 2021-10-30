f09b033c91162f13c63ae57300caf0af026d03ef
faberhurt <faberhurt@gmail.com>
2021-10-30 10:18:25 -0500

    Agregado Funcionalidad de agregar producto

--- src/pages/admin/Productos.jsx
+++ src/pages/admin/Productos.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const productosBackend = [
   {
@@ -25,6 +27,7 @@ const Productos = () => {
   const [mostrarTabla, setMostrarTabla] = useState(true);
   const [textoBoton, setTextoBoton] = useState("Agregar Producto");
   const [productos, setProductos] = useState([]);
+  const [colorBoton, setColorBoton] = useState("");
 
   useEffect(() => {
     setProductos(productosBackend);
@@ -33,8 +36,10 @@ const Productos = () => {
   useEffect(() => {
     if (mostrarTabla) {
       setTextoBoton("Agregar Producto");
+      setColorBoton("blue-900");
     } else {
       setTextoBoton("Mostrar Productos");
+      setColorBoton("green-500");
     }
   }, [mostrarTabla]);
 
@@ -48,7 +53,7 @@ const Productos = () => {
           onClick={() => {
             setMostrarTabla(!mostrarTabla);
           }}
-          className="bg-blue-900 p-3 text-white rounded-lg shadow-md hover:bg-blue-400 m-6 w-28 self-end"
+          className={`bg-${colorBoton} p-3 text-white rounded-lg shadow-md hover:bg-blue-400 m-6 w-28 self-end`}
         >
           {textoBoton}
         </button>
@@ -56,8 +61,13 @@ const Productos = () => {
       {mostrarTabla ? (
         <TablaProductos listaProductos={productos} />
       ) : (
-        <FormularioProductos />
+        <FormularioProductos
+          cambioTabla={setMostrarTabla}
+          listaProductos={productos}
+          AgregarProducto={setProductos}
+        />
       )}
+      <ToastContainer position="bottom-center" autoClose={1000} />
     </div>
   );
 };
@@ -80,78 +90,130 @@ const TablaProductos = ({ listaProductos }) => {
             <th>Estado Producto</th>
             <th>Descripción Producto</th>
           </tr>
-          </thead>
-          <tbody>
-
-              {listaProductos.map((producto)=>{
-                  return(
-                    <tr>
-                    <td>{producto.identificador}</td>
-                    <td>{producto.valor}</td>
-                    <td>{producto.estado}</td>
-                    <td>{producto.descripcion}</td>
-                  </tr>
-                  )
-              })}
-          </tbody>
+        </thead>
+        <tbody>
+          {listaProductos.map((producto) => {
+            return (
+              <tr>
+                <td>{producto.identificador}</td>
+                <td>{producto.valor}</td>
+                <td>{producto.estado}</td>
+                <td>{producto.descripcion}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
 };
 
-const FormularioProductos = () => {
+const FormularioProductos = ({
+  cambioTabla,
+  listaProductos,
+  AgregarProducto,
+}) => {
+  const [identificador, setIdentificador] = useState();
+  const [valor, setValor] = useState();
+  const [estado, setEstado] = useState();
+  const [descripcion, setDescripcion] = useState();
+
+  const GuardarProducto = () => {
+    console.log(
+      "identificador:",
+      identificador,
+      "valor:",
+      valor,
+      "estado",
+      estado,
+      "descripcion:",
+      descripcion
+    );
+    toast.success("Producto Guardado!", {
+      position: "bottom-center",
+      autoClose: 1000,
+    });
+    cambioTabla(true);
+    AgregarProducto([
+      ...listaProductos,
+      {
+        identificador: identificador,
+        valor: valor,
+        estado: estado,
+        descripcion: descripcion,
+      },
+    ]);
+  };
+
   return (
     <form className="flex flex-col w-full items-center justify-center">
       <h2 className="m-9 text-center text-3xl font-extrabold text-gray-900">
         Formulario de registro de productos
       </h2>
-      <input
-        className="border bg-gray-50 border-gray-900 p-2 rounded-lg m-2"
-        type="text"
-        name="identificador"
-        placeholder="Identificador del producto"
-      />
-      <input
-        className="border bg-gray-50 border-gray-600 p-2 rounded-lg m-2"
-        type="number"
-        name="valor"
-        placeholder="Valor Unitario"
-      />
-      <div class="block">
-        <span className="text-gray-700">Radio Buttons</span>
-        <div className="mt-2">
-          <div>
-            <label className="inline-flex items-center">
-              <input
-                type="radio"
-                className="form-radio"
-                name="estado"
-                value="available"
-                checked
-              />
-              <span className="ml-2">Disponible</span>
-            </label>
-          </div>
-          <div>
-            <label className="inline-flex items-center">
-              <input
-                type="radio"
-                className="form-radio"
-                name="estado"
-                value="unavailable"
-              />
-              <span class="ml-2">No Disponible</span>
-            </label>
-          </div>
-        </div>
-      </div>
-      <input
-        className="border bg-gray-50 border-gray-600 p-2 rounded-lg m-2"
-        type="text"
-        name="descripcion"
-        placeholder="Descripcion del producto"
-      />
-      <button className="bg-green-400 p-2 text-white rounded-lg shadow-md hover:bg-green-900">
+      <label htmlFor="identificador" className="flex flex-col items-center m-3">
+        Identificador del Producto
+        <input
+          className="border bg-gray-50 border-gray-900 p-2 rounded-lg m-2"
+          type="text"
+          name="identificador"
+          placeholder="Identificador del producto"
+          value={identificador}
+          onChange={(e) => {
+            setIdentificador(e.target.value);
+          }}
+        />
+      </label>
+      <label htmlFor="number" className="flex flex-col items-center m-3">
+        Valor Unitario
+        <input
+          className="border bg-gray-50 border-gray-600 p-2 rounded-lg m-2"
+          type="number"
+          min={0}
+          name="valor"
+          placeholder="Valor Unitario"
+          value={valor}
+          onChange={(e) => {
+            setValor(e.target.value);
+          }}
+        />
+      </label>
+
+      <label htmlFor="estado" className="flex flex-col items-center m-3">
+        Estado
+        <select
+          value={estado}
+          onChange={(e) => {
+            setEstado(e.target.value);
+          }}
+          className="bg-gray-50 border-gray-600 p-2 rounded-lg m-2"
+          name="estado"
+        >
+          <option disabled>Seleccione un Opción</option>
+          <option>Disponible</option>
+          <option>No Disponible</option>
+        </select>
+      </label>
+
+      <label htmlFor="descripcion" className="flex flex-col items-center m-3">
+        Descripcion del Producto
+        <input
+          className="border bg-gray-50 border-gray-600 p-2 rounded-lg m-2"
+          type="text"
+          name="descripcion"
+          placeholder="Descripcion del producto"
+          value={descripcion}
+          onChange={(e) => {
+            setDescripcion(e.target.value);
+          }}
+        />
+      </label>
+      <button
+        type="button"
+        className="bg-green-400 p-2 text-white rounded-lg shadow-md hover:bg-green-900"
+        onClick={() => {
+          GuardarProducto();
+        }}
+      >
         Registrar
       </button>
     </form>
