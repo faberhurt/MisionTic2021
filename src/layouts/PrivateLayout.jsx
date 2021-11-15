4906fc6003e47281edc6f94722c5022ba3c63a91
faberhurt <faberhurt@gmail.com>
2021-11-15 00:38:18 -0500

    sincro
    Se agregan manual debido a que no volvio a sincronizar

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,14 +1,19 @@
 import Sidebar from "../components/Sidebar";
 import React from "react";
+import PrivateRoute from "../components/PrivateRoute";
 
 const PrivateLayout = ({ children }) => {
   return (
-    <div className="flex w-screen h-screen">
-      <Sidebar />
-      <main className="flex w-full overflow-scroll items-center">
-        {children}
-      </main>
-    </div>
+    <PrivateRoute>
+      <div className="flex w-screen h-screen ">
+        <Sidebar />
+        {/* flex w-full overflow-scroll items-center*/}
+
+        <main className="flex flex-col w-full items-center justify-center overflow-y-scroll">
+          {children}
+        </main>
+      </div>
+    </PrivateRoute>
   );
 };
 export default PrivateLayout;
